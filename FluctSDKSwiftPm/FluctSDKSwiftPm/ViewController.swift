4cafb9fb8313a45899258658964118d27bdd58a5
arimura <33759663+arimura@users.noreply.github.com>
2024-01-15 18:58:42 +0900

    Format

--- FluctSDKSwiftPm/FluctSDKSwiftPm/ViewController.swift
+++ FluctSDKSwiftPm/FluctSDKSwiftPm/ViewController.swift
@@ -11,12 +11,12 @@ import GoogleMobileAds
 class ViewController: UIViewController, GADBannerViewDelegate {
     
     var bannerView: GADBannerView!
-
+    
     override func viewDidLoad() {
         // In this case, we instantiate the banner with desired ad size.
-           bannerView = GADBannerView(adSize: GADAdSizeBanner)
-
-           addBannerViewToView(bannerView)
+        bannerView = GADBannerView(adSize: GADAdSizeBanner)
+        
+        addBannerViewToView(bannerView)
         
         bannerView.adUnitID = "ca-app-pub-3265304037942335/4305558358"
         bannerView.rootViewController = self
@@ -25,48 +25,48 @@ class ViewController: UIViewController, GADBannerViewDelegate {
     }
     
     func addBannerViewToView(_ bannerView: GADBannerView) {
-       bannerView.translatesAutoresizingMaskIntoConstraints = false
-       view.addSubview(bannerView)
-       view.addConstraints(
-         [NSLayoutConstraint(item: bannerView,
-                             attribute: .bottom,
-                             relatedBy: .equal,
-                             toItem: view.safeAreaLayoutGuide,
-                             attribute: .bottom,
-                             multiplier: 1,
-                             constant: 0),
-          NSLayoutConstraint(item: bannerView,
-                             attribute: .centerX,
-                             relatedBy: .equal,
-                             toItem: view,
-                             attribute: .centerX,
-                             multiplier: 1,
-                             constant: 0)
-         ])
-      }
-  
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: view.safeAreaLayoutGuide,
+                                attribute: .bottom,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
+    
     func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
-      print("bannerViewDidReceiveAd")
+        print("bannerViewDidReceiveAd")
     }
-
+    
     func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
-      print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+        print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
     }
-
+    
     func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
-      print("bannerViewDidRecordImpression")
+        print("bannerViewDidRecordImpression")
     }
-
+    
     func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
-      print("bannerViewWillPresentScreen")
+        print("bannerViewWillPresentScreen")
     }
-
+    
     func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
-      print("bannerViewWillDIsmissScreen")
+        print("bannerViewWillDIsmissScreen")
     }
-
+    
     func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
-      print("bannerViewDidDismissScreen")
+        print("bannerViewDidDismissScreen")
     }
 }
 
